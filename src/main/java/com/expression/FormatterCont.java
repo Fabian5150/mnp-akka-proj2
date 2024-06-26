64d44591b10f7ad6b49bdff5da474d41ba8ce469
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2024-06-26 19:53:34 +0200

    FormatterCont class added

---
+++ src/main/java/com/expression/FormatterCont.java
@@ -0,0 +1,49 @@
+package com.expression;
+
+import akka.actor.typed.ActorRef;
+import akka.actor.typed.Behavior;
+import akka.actor.typed.javadsl.AbstractBehavior;
+import akka.actor.typed.javadsl.ActorContext;
+import akka.actor.typed.javadsl.Behaviors;
+import akka.actor.typed.javadsl.Receive;
+
+public class FormatterCont extends AbstractBehavior<FormatterCont.Message> {
+    //public interface Message {};
+    public record Message(/*ActorRef<FormatterCont.Message> cust,*/ String val) {}
+
+
+
+    private final ActorRef<FormatterCont.Message> cust;
+    private final String firstString;
+    private final String operation;
+
+    public static Behavior<Message> create(String first_value, String operation, ActorRef<Message> cust) {
+        return Behaviors.setup(context -> new FormatterCont(context, first_value,operation, cust));
+    }
+    private FormatterCont(ActorContext<FormatterCont.Message> context, String exp1, String operation, ActorRef<FormatterCont.Message> cust_)
+    {
+        super(context);
+        this.firstString= exp1;
+        this.cust= cust_;
+        this.operation = operation ;
+    }
+
+    @Override
+    public Receive<FormatterCont.Message> createReceive() {
+        return newReceiveBuilder()
+                .onMessage(Message.class, this::onMessage)
+                .build();
+    }
+
+    private Behavior<FormatterCont.Message> onMessage(FormatterCont.Message msg)
+    {
+        if(firstString==null)
+            return FormatterCont.create(msg.val, this.operation, this.cust);
+        else
+        {
+            this.cust.tell(new Message(firstString+" "+this.operation+" "+msg.val));
+
+            return this;
+        }
+    }
+}
