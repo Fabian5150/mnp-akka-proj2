814ce8b820f79edc8efbffc4447ec3dc21c3451c
ahhmed-hassan <ahmedhosam.1591@gmail.com>
2024-06-26 20:16:16 +0200

    onMsg method completed

--- src/main/java/com/expression/Formatter.java
+++ src/main/java/com/expression/Formatter.java
@@ -4,7 +4,6 @@ package com.expression;
 
 
 
-import akka.actor.typed.javadsl.TimerScheduler;
 import akka.actor.typed.Behavior;
 import akka.actor.typed.ActorRef;
 import akka.actor.typed.javadsl.AbstractBehavior;
@@ -16,8 +15,9 @@ import akka.actor.typed.javadsl.Receive;
 
 
 public class Formatter extends AbstractBehavior<Formatter.Message> {
-    public record Message(ActorRef<FormatterCont.Message> cust, Expression expr) {};
+    public record Message(ActorRef<FormatterCont.Message> cust, Expression expr) {}
 
+    private record SubExpressionInfo(Expression left, Expression right, String operation){}
     public static Behavior<Formatter.Message> create()
     {
         return Behaviors.setup(Formatter::new);
@@ -29,6 +29,18 @@ public class Formatter extends AbstractBehavior<Formatter.Message> {
     public Receive<Formatter.Message> createReceive() {
         return newReceiveBuilder().onMessage(Formatter.Message.class, this::onMsg).build();
     }
+    private SubExpressionInfo GetOperationFromExpression(Expression e)
+    {
+
+        if(e instanceof Expression.Add)
+            return new SubExpressionInfo(((Expression.Add) e).left(), ((Expression.Add) e).right(), "+");
+        if(e instanceof Expression.Mul)
+            return new SubExpressionInfo(((Expression.Mul) e).left(), ((Expression.Mul) e).right(),"*");
+        if(e instanceof Expression.Sub)
+            return new SubExpressionInfo(((Expression.Sub) e).left(), ((Expression.Sub) e).right(), "-");
+
+        throw new RuntimeException("Here only non primitive Expression! ");
+    }
 
     private Behavior<Formatter.Message> onMsg(Formatter.Message msg)
     {
@@ -37,7 +49,12 @@ public class Formatter extends AbstractBehavior<Formatter.Message> {
             String  value = String.valueOf( ((Expression.Val)msg.expr).inner() );
             msg.cust.tell(new FormatterCont.Message(value));
         }
-        //msg.cust.tell(new FormatterCont.Message ((Expression.Val) msg.expr).inner());
+        var info = GetOperationFromExpression(msg.expr);
+        ActorRef<FormatterCont.Message> cont= this.getContext().spawnAnonymous(
+                FormatterCont.create(null, info.operation, msg.cust));
+        this.getContext().getSelf().tell(new Formatter.Message(cont, info.left));
+        this.getContext().getSelf().tell(new Formatter.Message(cont, info.right));
+
         return this;
     }
 
